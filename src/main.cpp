b23a19de8a98e95d4cb555cf91535230922daba1
Xin Huang <huangxin@scutech.com>
2023-08-17 16:16:12 +0800

    add

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,32 @@
 #include<iostream>
 
 using namespace std;
+namespace std{
+    int x  =1;
+}
 
+namespace A{
+    int B = 3;
+}
+void func()
+{
+    long long str[0xffff];
+}
+void func5()
+{
+    long long str[UINT32_MAX];
+}
+class C
+{
+    const int a=1;
+};
 int main()
 {
-    cout<<"hello world"<<endl;
+    cout<<"hello"<<endl;
+    const char * str = "123";
+    const char * str1 = "123";
+    cout<<boolalpha<<(str == str1)<<endl;
+    char *p = "hello";
+    cout<<(str == p)<<endl;
+    cout<<(1>2) ? 4 : 3;
 }
